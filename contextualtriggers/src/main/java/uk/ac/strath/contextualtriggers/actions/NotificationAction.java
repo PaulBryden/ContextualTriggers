7b5be24134a96c85843b03c95bb6d52b03671e70
Paul Bryden <paul.bryden.2014@uni.strath.ac.uk>
2019-03-23 21:52:34 +0000

    Added formal Android notification and respective channels. Proof of Concept basic implementation complete

--- contextualtriggers/src/main/java/uk/ac/strath/contextualtriggers/actions/NotificationAction.java
+++ contextualtriggers/src/main/java/uk/ac/strath/contextualtriggers/actions/NotificationAction.java
@@ -1,20 +1,58 @@
 package uk.ac.strath.contextualtriggers.actions;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+
 import uk.ac.strath.contextualtriggers.Action;
 import uk.ac.strath.contextualtriggers.Logger;
+import uk.ac.strath.contextualtriggers.MainApplication;
+import uk.ac.strath.contextualtriggers.R;
+
+import static android.support.v4.content.ContextCompat.getSystemService;
 
 public class NotificationAction implements Action {
 
+    private static final String CHANNEL_ID = "contextualtriggers";
     private String message;
     private Logger logger;
     public NotificationAction(String message)
     {
         this.message = message;
         logger=Logger.getInstance();
+        createNotificationChannel();
+    }
+    
+    @Override
+    public void execute()
+    {
+        logger.log("*** SENDING NOTIFICATION ***\n\"" + message + "\"");
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(MainApplication.getAppContext(), CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("Notification")
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(MainApplication.getAppContext());
+// notificationId is a unique int for each notification that you must define
+        notificationManager.notify(0, builder.build());
     }
 
-    @Override
-    public void execute() {
-        logger.log("*** SENDING NOTIFICATION ***\n\"" + message + "\"");
+    private void createNotificationChannel()
+    {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "contextualtriggers";
+            String description = "contextualtriggers channel";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = (NotificationManager) getSystemService(MainApplication.getAppContext(),NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
