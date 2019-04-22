80be8ee983330777fc059bb18420a9562c6e521f
Paul Bryden <paul.bryden.2014@uni.strath.ac.uk>
2019-04-22 17:16:49 +0100

    Added all condition unit tests.

--- contextualtriggers/src/test/java/uk/ac/strath/contextualtriggers/ConditionUnitTest.java
+++ contextualtriggers/src/test/java/uk/ac/strath/contextualtriggers/ConditionUnitTest.java
@@ -19,7 +19,12 @@ import com.google.android.libraries.places.api.model.PlusCode;
 
 import org.junit.Test;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
@@ -33,8 +38,12 @@ import uk.ac.strath.contextualtriggers.conditions.GymNearbyCondition;
 import uk.ac.strath.contextualtriggers.conditions.HistoricStepsDaysUnmetCondition;
 import uk.ac.strath.contextualtriggers.conditions.InBuildingCondition;
 import uk.ac.strath.contextualtriggers.conditions.InBuildingTypeCondition;
+import uk.ac.strath.contextualtriggers.conditions.NoLongerInBuildingTypeCondition;
+import uk.ac.strath.contextualtriggers.conditions.NotNotifiedTodayCondition;
+import uk.ac.strath.contextualtriggers.conditions.StepAndGoalRealCountCondition;
 import uk.ac.strath.contextualtriggers.data.ActivityData;
 import uk.ac.strath.contextualtriggers.data.AltitudeData;
+import uk.ac.strath.contextualtriggers.data.DayData;
 import uk.ac.strath.contextualtriggers.data.PlacesData;
 import uk.ac.strath.contextualtriggers.data.StepAndGoalData;
 import uk.ac.strath.contextualtriggers.data.TimeOfDayData;
@@ -49,6 +58,7 @@ import static com.google.android.gms.awareness.fence.TimeFence.TIME_INTERVAL_AFT
 import static com.google.android.gms.awareness.fence.TimeFence.TIME_INTERVAL_MORNING;
 import static com.google.android.gms.location.DetectedActivity.STILL;
 import static org.junit.Assert.*;
+import static uk.ac.strath.contextualtriggers.conditions.StepAndGoalRealCountCondition.LESS_THAN;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -839,5 +849,322 @@ public class ConditionUnitTest
         manager.mock();
         assertEquals(true,altTransCondition.isSatisfied());
         System.out.println("InBuildingConditionUnitTest");
+    }
+
+
+    @Test
+    public void NoLongerInBuildingTypeConditionUnitTest()
+    {
+        class PlacesMockDataManager extends DataManager<PlacesData> implements IDataManager<PlacesData>
+        {
+            boolean firstTime=true;
+            @Nullable
+            @Override
+            public IBinder onBind(Intent intent)
+            {
+                return null;
+            }
+            public void mock()
+            {
+                List<PlaceLikelihood> data = new ArrayList<PlaceLikelihood>();
+                Place gym = new Place()
+                {
+                    @Override
+                    public int describeContents()
+                    {
+                        return 0;
+                    }
+
+                    @Override
+                    public void writeToParcel(Parcel parcel, int i)
+                    {
+
+                    }
+
+                    @Override
+                    public String getId()
+                    {
+                        return null;
+                    }
+
+
+                    @Nullable
+                    @Override
+                    public String getAddress()
+                    {
+                        return null;
+                    }
+
+                    @Nullable
+                    @Override
+                    public AddressComponents getAddressComponents()
+                    {
+                        return null;
+                    }
+
+                    @Nullable
+                    @Override
+                    public List<String> getAttributions()
+                    {
+                        return null;
+                    }
+
+                    @Nullable
+                    @Override
+                    public OpeningHours getOpeningHours()
+                    {
+                        return null;
+                    }
+
+                    @Nullable
+                    @Override
+                    public String getPhoneNumber()
+                    {
+                        return null;
+                    }
+
+                    @Override
+                    public LatLng getLatLng()
+                    {
+                        return null;
+                    }
+
+                    @Nullable
+                    @Override
+                    public String getName()
+                    {
+                        return null;
+                    }
+
+                    @Override
+                    public LatLngBounds getViewport()
+                    {
+                        return null;
+                    }
+
+                    @Override
+                    public Uri getWebsiteUri()
+                    {
+                        return null;
+                    }
+
+                    @Nullable
+                    @Override
+                    public List<PhotoMetadata> getPhotoMetadatas()
+                    {
+                        return null;
+                    }
+
+                    @Nullable
+                    @Override
+                    public PlusCode getPlusCode()
+                    {
+                        return null;
+                    }
+
+                    @Nullable
+                    @Override
+                    public Integer getPriceLevel()
+                    {
+                        return null;
+                    }
+
+                    @Nullable
+                    @Override
+                    public Double getRating()
+                    {
+                        return null;
+                    }
+
+                    @Nullable
+                    @Override
+                    public List<Type> getTypes()
+                    {
+                        ArrayList<Type> types = new ArrayList<Type>();
+                        types.add(Type.CAFE);
+                        return types;
+                    }
+
+                    @Nullable
+                    @Override
+                    public Integer getUserRatingsTotal()
+                    {
+                        return null;
+                    }
+                };
+                PlaceLikelihood likelihood = new PlaceLikelihood()
+                {
+
+                    @Override
+                    public int describeContents()
+                    {
+                        return 0;
+                    }
+
+                    @Override
+                    public void writeToParcel(Parcel parcel, int i)
+                    {
+
+                    }
+
+                    @Override
+                    public Place getPlace()
+                    {
+                        return gym;
+                    }
+
+                    @Override
+                    public double getLikelihood()
+                    {
+                        return 0.76;
+                    }
+                };
+
+                PlaceLikelihood likelihood2 = new PlaceLikelihood()
+                {
+
+                    @Override
+                    public int describeContents()
+                    {
+                        return 0;
+                    }
+
+                    @Override
+                    public void writeToParcel(Parcel parcel, int i)
+                    {
+
+                    }
+
+                    @Override
+                    public Place getPlace()
+                    {
+                        return gym;
+                    }
+
+                    @Override
+                    public double getLikelihood()
+                    {
+                        return 0.1;
+                    }
+                };
+                if(firstTime)
+                {
+                    data.add(likelihood);
+                    firstTime=false;
+                }
+                else
+                {
+                    data.remove(likelihood);
+                    data.add(likelihood2);
+                }
+                sendUpdate(new PlacesData(data));
+            }
+        }
+        UnitTestAction action = new UnitTestAction();
+        PlacesMockDataManager manager = new PlacesMockDataManager();
+        NoLongerInBuildingTypeCondition altTransCondition = new NoLongerInBuildingTypeCondition(Place.Type.CAFE,manager);
+        Trigger.Builder T = new Trigger.Builder();
+        T.setCondition(altTransCondition);
+        T.setAction(action);
+        Trigger trig = T.build();
+        manager.mock();
+        assertEquals(false,altTransCondition.isSatisfied());
+        manager.mock();
+        assertEquals(true,altTransCondition.isSatisfied());
+        System.out.println("NoLongerInBuildingConditionUnitTest");
+    }
+
+    @Test
+    public void NotNotifiedTodayConditionUnitTest() {
+        class MockNotificationDataManager extends DataManager<VoidData> implements IDataManager<VoidData>
+        {
+            public MockNotificationDataManager()
+            {
+            }
+            @Nullable
+            @Override
+            public IBinder onBind(Intent intent)
+            {
+                return null;
+            }
+            public void mock()
+            {
+                sendUpdate(new VoidData());
+            }
+        }
+
+        UnitTestAction action = new UnitTestAction();
+        MockNotificationDataManager manager = new MockNotificationDataManager();
+        NotNotifiedTodayCondition altTransCondition = new NotNotifiedTodayCondition(manager);
+        Trigger.Builder T = new Trigger.Builder();
+        T.setCondition(altTransCondition);
+        T.setAction(action);
+        Trigger trig = T.build();
+        assertEquals(true,altTransCondition.isSatisfied());
+        manager.mock();
+        assertEquals(false,altTransCondition.isSatisfied());
+        System.out.println("NotNotifiedTodayConditionUnitTest");
+
+    }
+
+
+    @Test
+    public void StepAndGoalRealCountConditionUnitTest() {
+        class MockStepAndGoalCountManager extends DataManager<StepAndGoalData> implements IDataManager<StepAndGoalData>
+        {
+
+            public MockStepAndGoalCountManager()
+            {
+                firstTime=true;
+            }
+            boolean firstTime;
+            @Nullable
+            @Override
+            public IBinder onBind(Intent intent)
+            {
+                return null;
+            }
+            public void mock()
+            {
+                if(firstTime)
+                {
+                    sendUpdate(new StepAndGoalData());
+                    firstTime=false;
+                }
+                else
+                {
+                    StepAndGoalData fulfilled = new StepAndGoalData();
+                    DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+                    Calendar cal0 = Calendar.getInstance();
+                    Date today = cal0.getTime();
+                    try
+                    {
+                        today = formatter.parse(formatter.format(today));
+                    }
+                    catch (ParseException e)
+                    {
+                        e.printStackTrace();
+                    }
+                    DayData day = fulfilled.getDay(today);
+                    day.steps=100000;
+                    fulfilled.updateDay(day);
+                    sendUpdate(fulfilled);
+                }
+            }
+        }
+
+        UnitTestAction action = new UnitTestAction();
+        MockStepAndGoalCountManager manager = new MockStepAndGoalCountManager();
+        StepAndGoalRealCountCondition altTransCondition = new StepAndGoalRealCountCondition(LESS_THAN,manager);
+        Trigger.Builder T = new Trigger.Builder();
+        T.setCondition(altTransCondition);
+        T.setAction(action);
+        Trigger trig = T.build();
+        manager.mock();
+        assertEquals(true,altTransCondition.isSatisfied());
+        manager.mock();
+        assertEquals(false,altTransCondition.isSatisfied());
+        System.out.println("NotNotifiedTodayConditionUnitTest");
+
     }
 }
