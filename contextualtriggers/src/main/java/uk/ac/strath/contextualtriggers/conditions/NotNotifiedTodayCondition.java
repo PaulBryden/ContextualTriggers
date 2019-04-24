6acd89d1a71d01f1d41e4aa87dba3f662008fd81
R. David Dunphy <rdd.dunphy@gmail.com>
2019-04-24 18:03:32 +0100

    Remove data duplication

--- contextualtriggers/src/main/java/uk/ac/strath/contextualtriggers/conditions/NotNotifiedTodayCondition.java
+++ contextualtriggers/src/main/java/uk/ac/strath/contextualtriggers/conditions/NotNotifiedTodayCondition.java
@@ -1,18 +1,9 @@
 package uk.ac.strath.contextualtriggers.conditions;
 
-import com.google.android.libraries.places.api.model.Place;
-import com.google.android.libraries.places.api.model.PlaceLikelihood;
-
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
-import java.util.Calendar;
-import java.util.Date;
 
-import uk.ac.strath.contextualtriggers.data.PlacesData;
 import uk.ac.strath.contextualtriggers.data.VoidData;
 import uk.ac.strath.contextualtriggers.managers.IDataManager;
 
@@ -23,11 +14,9 @@ import uk.ac.strath.contextualtriggers.managers.IDataManager;
  */
 public class NotNotifiedTodayCondition extends DataCondition<VoidData>
 {
-    LocalDate date;
     public NotNotifiedTodayCondition(IDataManager<VoidData> dataManager)
     {
         super(dataManager);
-        date=LocalDate.ofEpochDay(0);
     }
 
     @Override
@@ -38,13 +27,16 @@ public class NotNotifiedTodayCondition extends DataCondition<VoidData>
     @Override
     public void notifyUpdate(VoidData data)
     {
-        date = LocalDate.now();
         super.notifyUpdate(data);
     }
 
     @Override
     public boolean isSatisfied()
     {
+        if (getData() == null) {
+            return true;
+        }
+        LocalDate date = Instant.ofEpochMilli(getData().getTimestamp()).atZone(ZoneId.systemDefault()).toLocalDate();
         return !date.equals(LocalDate.now());
     }
 }
